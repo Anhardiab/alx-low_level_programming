839db7f0a411032a4ec39fc345f24a4353cf3065
anhardiab <rivadiab30663@gmail.com>
2023-05-29 09:15:35 -0700

    first commit

--- 0x09-static_libraries/1-isdigit.c
+++ 0x09-static_libraries/1-isdigit.c
@@ -1,11 +1,17 @@
 #include "main.h"
 
 /**
- * _isdigit - checks for a digit (0 through 9)
- * @c: int to be checked
+ * _isdigit - checks whether a given input is a digit
+ * @c: a parameter
+ * @c: a parameter
  * Return: 1 if c is a digit, 0 otherwise
  */
 int _isdigit(int c)
 {
-	return (c >= '0' && c <= '9');
+	if (c >= 48 && c <= 57)
+	{
+		return (1);
+	}
+	else
+		return (0);
 }
